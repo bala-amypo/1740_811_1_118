8322382842774831e91ab3e76dd6df535b8c2088
bala-amypo <bala@amypo.in>
2025-12-21 06:10:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/SupplierProfileController.java
+++ src/main/java/com/example/demo/controller/SupplierProfileController.java
@@ -1,7 +1,8 @@
-
-import jakarta
+package com.example.demo.controller
+import org.springframework.web.bind.annotation.*;
 @RestController
 class SupplierProfileController{
+    @PostMapping("/")
 
 
 
