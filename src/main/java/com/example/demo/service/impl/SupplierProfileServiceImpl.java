5ce8b7f3dbc098dd3a77f9ab202408dba7006224
bala-amypo <bala@amypo.in>
2025-12-22 08:33:49 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplierProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplierProfileServiceImpl.java
@@ -30,7 +30,7 @@ public class SupplierProfileServiceImpl implements SupplierProfileService {
     
     @Override
     public Optional<SupplierProfile> getBySupplierCode(String supplierCode) {
-        if(supplierCode == null || supplierCode.trim().isEmpty()){
+        if(supplierCode == null || supplierCode.trim().isEmpty() || supplierCode.equals("000") ){
             throw new BadRequestException("supplierCode must not be Zero" + supplierCode);
         }
         return supplierProfileRepository.findBySupplierCode(supplierCode);
