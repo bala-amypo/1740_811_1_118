7e4950f799c5e59f36302d9e1c42106d55ac216a
bala-amypo <bala@amypo.in>
2025-12-22 09:45:49 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "supplier_profiles")
 public class SupplierProfile {
-    
+       
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
