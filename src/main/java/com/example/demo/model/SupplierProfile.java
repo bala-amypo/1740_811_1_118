724e0efb9a0e67aa9d8c09f41a2af31241f9f340
bala-amypo <bala@amypo.in>
2025-12-23 07:59:42 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -1,9 +1,7 @@
 package com.example.demo.model;
 
 import java.time.LocalDateTime;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence. 
+import jakarta.persistence.*; 
 @Entity
 @Table(name = "supplier_profiles")
 public class SupplierProfile {
