57b535c2d21b12301b0a02560c591207339b82d4
bala-amypo <bala@amypo.in>
2025-12-27 06:09:10 +0000

    User commit

--- src/main/java/com/example/demo/service/PurchaseOrderService.java
+++ src/main/java/com/example/demo/service/PurchaseOrderService.java
@@ -1,13 +1,15 @@
 package com.example.demo.service;
 
-import com.example.demo.model.DeliveryRecord;
+import com.example.demo.model.PurchaseOrderRecord;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface DeliveryRecordService {
-    DeliveryRecord recordDelivery(DeliveryRecord delivery);
-    List<DeliveryRecord> getDeliveriesByPO(Long poId);
-    List<DeliveryRecord> getAllDeliveries();
+public interface PurchaseOrderService {
+    PurchaseOrderRecord createPurchaseOrder(PurchaseOrderRecord po);
+    List<PurchaseOrderRecord> getPOsBySupplier(Long supplierId);
+    Optional<PurchaseOrderRecord> getPOById(Long id);
+    List<PurchaseOrderRecord> getAllPurchaseOrders();
 }
 
 
